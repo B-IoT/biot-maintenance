8813dad54d6bf301374f4a20c8c6498b45f239ff
LudoHoff <ludo.hoffstetter@epfl.ch>
2021-03-15 10:46:08 +0100

    Shift gps coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,9 @@ function App() {
         markers.push(new LngLat(evt.lngLat[0], evt.lngLat[1]))
     }
 
+    const lonShift = -0.00000804584146*0.3;
+    const latShift = -0.00002940280387*0.8;
+
     return (
         <div className="container">
             <ReactMapGl
@@ -57,24 +60,24 @@ function App() {
                     type="image"
                     url={blueprint}
                     coordinates={[
-                        [6.6221646113321135, 46.52992292443224],
-                        [6.623385456772448, 46.52943385229428],
-                        [6.622987925323761, 46.528960050318986],
-                        [6.621764919250702, 46.52944756749944]
+                        [6.6221646113321135 + lonShift, 46.52992292443224 + latShift],
+                        [6.623385456772448 + lonShift, 46.52943385229428 + latShift],
+                        [6.622987925323761 + lonShift, 46.528960050318986 + latShift],
+                        [6.621764919250702 + lonShift, 46.52944756749944 + latShift]
                     ]}
                 />
                 <Layer
                     id="overlay"
                     source="map-source"
                     type="raster"
-                    paint={{ "raster-opacity": 0.85 }}
+                    paint={{ "raster-opacity": 0.05 }}
                 />
                 {markers.map((gps, idx) =>
                     <MapMarker id={idx} gps={gps} onDelete={() => markers.splice(idx, 1)}/>)}
             </ReactMapGl>
             <div className="export-container">
                 {markers.map((gps, idx) =>
-                    <div>{'RELAIS ' + idx} <br/>  {' lon: ' + gps.longitude} <br/> {'lat: ' + gps.latitude} <br/> <br/> g</div>)}
+                    <div>{'RELAIS ' + (idx + 1)} <br/>  {' lon: ' + gps.longitude} <br/> {'lat: ' + gps.latitude} <br/> <br/> </div>)}
             </div>
         </div>
     );
